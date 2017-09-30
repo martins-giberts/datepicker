47870d3b41efaef41abcbef1e19341094ef1470f
Martins Giberts <martins.giberts@gmail.com>
2017-09-30 11:59:07 +0200

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: 'airbnb',
+  root: true,
+  parser: 'babel-eslint',
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      generators: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  settings: {
+    'import/extensions': ['.js'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.json']
+      }
+    }
+  },
+  rules: {
+    'object-curly-spacing': ['error', 'never'],
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js'] }],
+  }
+}
